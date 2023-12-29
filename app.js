07026b0951a213ee0f05d1890c1ebaea61097c8a
drake alexander <drakefoster498@gmail.com>
2023-12-29 04:38:05 +0000

    changed text

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 'use strict';
 
 let name=prompt('Please let us know who you are;')
-            document.write('<p>Your name is ',name,'</p>');
+            document.write('<p> Hi ',name,' welcome to my page!</p>');
             confirm('are you sure?')
 
             if(confirm('Do these pop ups annoy you?')){
-                document.write('<p>Okay I will remove them</p>');
+                document.write('<p>Okay I will remove the pop ups.</p>');
             } else {
                 document.write('<p> Thanks! I will add more!</p>');
             }
-            
