e566fe05548f2594804901e6d10924336089c0fd
drake alexander <drakefoster498@gmail.com>
2024-01-17 01:25:00 +0000

    lots of changes

--- app.js
+++ app.js
@@ -2,17 +2,29 @@
 
 function username() {
     const username=prompt('Please let us know who you are;')
-        if (username =="") { prompt("How about a code name instead?")
+        while (username =="") { prompt("How about a code name instead?")
             console.log(username);
             document.write('<p> Hi ',username,' welcome to my page!</p>');
-        } else 
-            document.write('<p> Hi ',username,' welcome to my page!</p>');
-        
+        } }
+username()
 
-            if(confirm('Do these pop ups annoy you?')){
-                document.write('<p>Okay I will remove the pop ups.</p>');
-            } else {
-                document.write('<p> Thanks! I will add more!</p>');
-            }
-        }
+function addition(){
+    let addition = prompt (' Think fast! What is 176 + 32?');
+    const correct = 208
+    
+     if (addition !== correct)
+        while (addition != correct){
+               addition = prompt('Please try again. What is 176 + 32?');
+}
+    
+if (addition == correct) {
+    alert('<p> You did it! Great job!</p>')
+}}
+addition()
 
+
+function ratemypage(){
+let rating = prompt('On a scale of 1-5 how much do you like coding?');
+for (let i = 0; i < rating; i++){
+    document.write('<img src="funny error.jpg" alt="funny image about coding errors")/>');
+}}
